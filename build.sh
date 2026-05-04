def8ac2de2728fbd7f1a7df16c8e4098edbabfd5
maska1981-bri <maska1981@hotmail.it>
2026-05-04 22:33:13 +0200

    checkpoint stabile prima demo

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='demo').exists() or User.objects.create_superuser('demo', 'demo@example.com', 'Demo123!')" | python manage.py shell
